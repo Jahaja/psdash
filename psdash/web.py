1258759c07622d4fb133bc7a04e7fd2b8049ab37
Joakim Hamren <joakim.hamren@gmail.com>
2014-04-02 19:16:45 +0200

    Fixed issue where file size formatted values would be displayed as zero.
    This was caused by an bug present in Jinja:
    https://github.com/mitsuhiko/jinja2/pull/59

--- psdash/web.py
+++ psdash/web.py
@@ -62,6 +62,39 @@ if not app.secret_key:
 allowed_remote_addrs = []
 
 
+# Patch the built-in, but not working, filesizeformat filter for now.
+# See https://github.com/mitsuhiko/jinja2/pull/59 for more info.
+@app.template_filter()
+def filesizeformat(value, binary=False):
+    """Format the value like a 'human-readable' file size (i.e. 13 kB,
+    4.1 MB, 102 Bytes, etc).  Per default decimal prefixes are used (Mega,
+    Giga, etc.), if the second parameter is set to `True` the binary
+    prefixes are used (Mebi, Gibi).
+    """
+    bytes = float(value)
+    base = binary and 1024 or 1000
+    prefixes = [
+        (binary and 'KiB' or 'kB'),
+        (binary and 'MiB' or 'MB'),
+        (binary and 'GiB' or 'GB'),
+        (binary and 'TiB' or 'TB'),
+        (binary and 'PiB' or 'PB'),
+        (binary and 'EiB' or 'EB'),
+        (binary and 'ZiB' or 'ZB'),
+        (binary and 'YiB' or 'YB')
+    ]
+    if bytes == 1:
+        return '1 Byte'
+    elif bytes < base:
+        return '%d Bytes' % bytes
+    else:
+        for i, prefix in enumerate(prefixes):
+            unit = base ** (i + 2)
+            if bytes < unit:
+                return '%.1f %s' % ((base * bytes / unit), prefix)
+        return '%.1f %s' % ((base * bytes / unit), prefix)
+
+
 @app.before_first_request
 def load_allowed_remote_addrs():
     addrs = app.config.get("PSDASH_ALLOWED_REMOTE_ADDRESSES")
